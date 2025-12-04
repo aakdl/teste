ceae592f141ec8565978d09bc57063f55362fc89
aakdl <148289397+aakdl@users.noreply.github.com>
2025-12-03 22:55:33 -0300

    Add files via upload

---
+++ pasta /scr/pages/Dashboard.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Dashboard() {
+  return (
+    <div style={{ padding: 20 }}>
+      <h1>Dashboard PPC</h1>
+      <p>Resumo e métricas vão aparecer aqui...</p>
+    </div>
+  );
+}
