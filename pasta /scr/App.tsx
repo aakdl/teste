58a3e23a7c1ff816c80fd1c6be556b0436506e0c
aakdl <148289397+aakdl@users.noreply.github.com>
2025-12-03 22:56:30 -0300

    Add files via upload

---
+++ pasta /scr/App.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Activities from "./pages/Activities";
+import Header from "./components/Header";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Activities />
+    </>
+  );
+}
+
+export default App;
